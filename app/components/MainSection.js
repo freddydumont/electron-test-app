2b261b82a7b7bea404dfca6951928530cbb461aa
Unknown <frederickmorin18@gmail.com>
2018-06-22 01:01:08 -0400

    Style main section and extract it from App component

---
+++ app/components/MainSection.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Section, Tile, Box } from 'bloomer';
+import SidebarMenu from './SidebarMenu';
+
+const Main = () => (
+  <main>
+    <Section
+      style={{ height: 'calc(100vh - 3.25rem)' }}
+      className="has-background-light"
+    >
+      <Tile isAncestor style={{ height: 'calc(100vh - 3.25rem - 72px)' }}>
+        <Tile isParent>
+          <Tile isSize={2} isParent>
+            <Tile
+              isChild
+              render={props => (
+                <Box {...props}>
+                  <SidebarMenu />
+                </Box>
+              )}
+            />
+          </Tile>
+          <Tile isSize={7} isParent>
+            <Tile
+              isChild
+              render={props => (
+                <Box {...props}>
+                  <p className="menu-label">Products</p>
+                </Box>
+              )}
+            />
+          </Tile>
+          <Tile isSize={3} isParent>
+            <Tile
+              isChild
+              render={props => (
+                <Box {...props}>
+                  <p className="menu-label">Invoice</p>
+                </Box>
+              )}
+            />
+          </Tile>
+        </Tile>
+      </Tile>
+    </Section>
+  </main>
+);
+
+export default Main;
--- app/containers/App.js
+++ app/containers/App.js
@@ -1,20 +1,15 @@
 // @flow
 import * as React from 'react';
 // import { Switch, Route } from 'react-router';
-import { Section } from 'bloomer';
-// import HomePage from '../containers/HomePage';
 import Nav from '../components/Navbar';
-import SidebarMenu from '../components/SidebarMenu';
+import Main from '../components/MainSection';
 
 export default class App extends React.Component {
   render() {
     return (
       <React.Fragment>
         <Nav />
-        <Section style={{ height: 'calc(100vh - 3.25rem)' }}>
-          <SidebarMenu />
-          <h2>App</h2>
-        </Section>
+        <Main />
       </React.Fragment>
     );
   }
